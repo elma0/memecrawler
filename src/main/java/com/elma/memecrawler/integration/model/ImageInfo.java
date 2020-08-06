22c700b6c4e54ab4e6771fc5863d1f070fb6c56d
eldar.mambetov <eldar.mambetov@piano.io>
2020-08-06 23:27:04 +0400

    retry added

--- src/main/java/com/elma/memecrawler/integration/model/ImageInfo.java
+++ src/main/java/com/elma/memecrawler/integration/model/ImageInfo.java
@@ -1,12 +1,41 @@
 package com.elma.memecrawler.integration.model;
 
-import java.util.UUID;
-
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Document(collection = "image_info")
-public record ImageInfo(UUID id, String name, String url) {
-    public ImageInfo(String name, String url) {
-        this(null, name, url);
+public class ImageInfo {
+    @Id
+    @Field("_id")
+    @JsonIgnore
+    private String id;
+    private String name;
+
+    public ImageInfo(String name) {
+        this.name = name;
+    }
+
+    public ImageInfo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
